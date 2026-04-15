2231e3d4620399179329b4803ade87c263e5db80
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-04-15 12:23:47 -0700

    trying to figure it out... talking to database and server listening
    - set up database and got migrations talking to it through package.json creating a dbMigration script
    - can open a server port on 3000 per environment variable and defaults to 5000 when env missing
    - originally tried to use Prisma but that got messy so we are going with manual database manipulation for now

---
+++ server/db/pool.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg")
+
+module.exports = new Pool({
+    host: "localhost",
+    user: chris,
+    database: "smart_task_auditor",
+    password: pgdbpass,
+    port: 5432
+})
